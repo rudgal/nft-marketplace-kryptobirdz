b1559d5941b9f5f09c23190226b84561e4fbfd4d
rudi <rudi@vorsicht-bissig.de>
2021-12-07 20:06:00 +0100

    26. Loading NFTs To The Market Function

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,46 @@
+import { ethers } from 'ethers';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+import { nftaddress, nftmarketaddress } from '../config';
+
 export default function Home() {
+  const [nfts, setNFTs] = useState([]);
+  const [loadingState, setLoadingState] = useState('not-loaded');
+
+  useEffect(() => {
+    loadNFTs();
+  }, []);
+
+  async function loadNFTs() {
+    // what we want to load
+    // ***provider, tokenContract, marketcontract, data for our marketItems
+
+    const provider = new ethers.providers.JsonRpcProvider();
+    const tokenContract = new ethers.Contract(nftaddress.NFT.abi, provider);
+    const marketContract = new ethers.Contract(nftmarketaddress.Market.abi, provider);
+    const data = await marketContract.fetchMarketItems();
+
+    const items = await Promise.all(
+      data.map(async (i) => {
+        const tokenUri = await tokenContract.tokenURI(i.tokenId);
+        // we want to get the token metadata
+        const meta = await axios.get(tokenUri);
+        let price = ethers.utils.formatUnits(i.price.toString(), 'ether');
+        let item = {
+          price,
+          tokenId: i.tokenId.toNumber(),
+          seller: i.seller,
+          owner: i.owner,
+          image: meta.data.image,
+          name: meta.data.name,
+          description: meta.data.description,
+        };
+        return item;
+      })
+    );
+    setNFTs(items);
+  }
+
   return <div></div>;
 }
