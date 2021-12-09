d6a3beac26dca4858a96a01fbefcbccec42c68a7
rudi <rudi@vorsicht-bissig.de>
2021-12-09 09:07:31 +0100

    27. Buy NFTs Function

--- pages/index.js
+++ pages/index.js
@@ -18,7 +18,7 @@ export default function Home() {
 
     const provider = new ethers.providers.JsonRpcProvider();
     const tokenContract = new ethers.Contract(nftaddress.NFT.abi, provider);
-    const marketContract = new ethers.Contract(nftmarketaddress.Market.abi, provider);
+    const marketContract = new ethers.Contract(nftmarketaddress.KBMarket.abi, provider);
     const data = await marketContract.fetchMarketItems();
 
     const items = await Promise.all(
@@ -40,6 +40,23 @@ export default function Home() {
       })
     );
     setNFTs(items);
+    setLoadingState('loaded');
+  }
+
+  // function to buy nfts for market
+
+  async function buyNFT(nft) {
+    const web3Modal = new Web3Modal();
+    const connection = await web3Modal.connect;
+    const provider = new ethers.providers.Web3Provider(connection);
+    const signer = provider.getSigner();
+    const contract = new ethers.Contract(nftmarketaddress, KBMarket.abi, signer);
+
+    const price = ethers.utils.parseUnits(nft.price.toString(), 'ether');
+    const transaction = await contract.createMarketSale(nftaddress, nft.tokenId, { value: price });
+
+    await transaction.wait();
+    loadNFTs();
   }
 
   return <div></div>;
