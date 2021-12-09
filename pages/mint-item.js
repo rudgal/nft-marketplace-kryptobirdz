667e0130c1fd6bf64ae7c65519aa3704fce9cf2b
rudi <rudi@vorsicht-bissig.de>
2021-12-09 09:57:09 +0100

    31. Front End Form For Blockchain Interaction

--- pages/mint-item.js
+++ pages/mint-item.js
@@ -74,4 +74,31 @@ export default function MintItem() {
     await transaction.wait();
     router.push('./');
   }
+
+  return (
+    <div className="flex justify-center">
+      <div className="w-1/2 flex flex-col pb-12">
+        <input
+          placeholder="Asset Name"
+          className="mt-8 border rounded p-4"
+          onChange={(e) => updateFormInput({ ...formInput, name: e.target.value })}
+        />
+        <textarea
+          placeholder="Asset Description"
+          className="mt-2 border rounded p-4"
+          onChange={(e) => updateFormInput({ ...formInput, description: e.target.value })}
+        />
+        <input
+          placeholder="Asset Price in eth"
+          className="mt-2 border rounded p-4"
+          onChange={(e) => updateFormInput({ ...formInput, price: e.target.value })}
+        />
+        <input type="file" placeholder="Asset" className="mt-4" onChange={onChange} />
+        {fileUrl && <img className="rounded mt-4" width="350px" src={fileUrl} />}
+        <button onClick={createMarket} className="font-bold mt-4 bg-purple-500 text-white rounded p-4 shadow-lg">
+          Mint NFT
+        </button>
+      </div>
+    </div>
+  );
 }
