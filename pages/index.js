afdbb4bda663dd58fc6f6c242770e6c1a7ac14e9
rudi <rudi@vorsicht-bissig.de>
2021-12-09 09:23:46 +0100

    28. Mapping Out NFTs with Tailwind & NexJs

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,12 @@
 import { ethers } from 'ethers';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
+import Web3Modal from 'web3modal';
 
 import { nftaddress, nftmarketaddress } from '../config';
+
+import NFT from '../artifacts/contracts/NFT.sol/NFT.json';
+import KBMarket from '../artifacts/contracts/KBMarket.sol/KBMarket.json';
 
 export default function Home() {
   const [nfts, setNFTs] = useState([]);
@@ -17,9 +21,9 @@ export default function Home() {
     // ***provider, tokenContract, marketcontract, data for our marketItems
 
     const provider = new ethers.providers.JsonRpcProvider();
-    const tokenContract = new ethers.Contract(nftaddress.NFT.abi, provider);
-    const marketContract = new ethers.Contract(nftmarketaddress.KBMarket.abi, provider);
-    const data = await marketContract.fetchMarketItems();
+    const tokenContract = new ethers.Contract(nftaddress, NFT.abi, provider);
+    const marketContract = new ethers.Contract(nftmarketaddress, KBMarket.abi, provider);
+    const data = await marketContract.fetchMarketTokens();
 
     const items = await Promise.all(
       data.map(async (i) => {
@@ -59,5 +63,35 @@ export default function Home() {
     loadNFTs();
   }
 
-  return <div></div>;
+  if (loadingState === 'loaded' && !nfts.length) {
+    return <h1 className="px-20 py-7 text-4x1">No NFTs in marketplace</h1>;
+  }
+
+  return (
+    <div className="flex justify-center">
+      <div className="px-4" style={{ maxWidth: '160px' }}></div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:gird-cols-4 gap-4 pt-4" style={{ maxWidth: '160px' }}>
+        {nfts.map((nft, i) => (
+          <div key={i} className="border shadow rounded-x1 overflow-hidden">
+            <img src={nft.image} />
+            <div className="p-4">
+              <p style={{ height: '64px' }} className="text-3x1 font-semibold">
+                {nft.name}
+              </p>
+              <div style={{ height: '72px', overflow: 'hidden' }}>
+                <p className="text-gray-400">{nft.description}</p>
+              </div>
+            </div>
+            <div className="p-4 bg-black">
+              <p className="text-3x-1 mb-4 font-bold text-white">{nft.price} ETH</p>
+              <button
+                className="w-full bg-purple-500 text-white font-boid py-3 px-12 rounded"
+                onClick={() => buyNFT(nft)}
+              ></button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
